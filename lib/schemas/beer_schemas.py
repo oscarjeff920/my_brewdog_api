99c5a87d64f0165b23b07236323f1d63c794de1d
Oscar Jeffery <oscarjjeff920@gmail.com>
2022-05-24 07:31:01 -0300

    added schemas for specific api calls to only return relevent data

--- lib/schemas/beer_schemas.py
+++ lib/schemas/beer_schemas.py
@@ -1,18 +1,42 @@
 from pydantic import BaseModel as _BaseModel
 import typing as _typing
-from lib.schemas.pydantic_schemas import Ingredients as _Ingredients
-from lib.schemas.pydantic_schemas import ColourScale as _ColourScale
+from lib.schemas.sub_nested_schemas import Ingredients as _Ingredients, Malt, Hops
+from lib.schemas.sub_nested_schemas import ColourScale as _ColourScale
 
 
-class FullBeerBase(_BaseModel):
+class BeerBase(_BaseModel):
     id: int
     name: str
     tagline: str
+    image_url: _typing.Optional[str] = None
+
+
+class FullBeerBase(BeerBase):
     first_brewed: str
     description: str
-    image_url: str
     abv: float
     ingredients: _Ingredients
     colour_rating: _ColourScale
     ibu: _typing.Optional[int] = None
     ph: _typing.Optional[float] = None
+
+
+class AllBeerBase(BeerBase):
+    description: str
+    abv: float
+
+
+class AbvBeerBase(BeerBase):
+    abv: float
+
+
+class MaltBeerBase(BeerBase):
+    malt: _typing.List[Malt]
+
+
+class HopsBeerBase(BeerBase):
+    hops: _typing.List[Hops]
+
+
+class YeastBeerBase(BeerBase):
+    yeast: str
